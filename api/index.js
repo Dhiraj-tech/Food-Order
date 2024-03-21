726dab957a8fde362ea56f2d0067e6178a121ffa
Dhiraj-tech <dhirajrayofficial@gmail.com>
2024-03-21 18:38:15 +0545

    committed

--- api/index.js
+++ api/index.js
@@ -9,9 +9,7 @@ config()
 
 const app = express()
 
-app.use(cors({
-    origin: 'https://food-order-qnl2.onrender.com'
-  }));
+app.use(cors());
 
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended: true}))
